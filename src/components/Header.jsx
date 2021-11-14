c2a9aa74dd3ab560d4ef4caa460dfeb1857c1ea5
Jamie Anderson <jamie@cryptnology.dev>
2021-11-14 17:56:11 +1100

    Altered Header to be better responsive and added links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,65 +18,75 @@ const Header = ({
   };
 
   return (
-    <Navbar collapseOnSelect expand='md' fixed='top'>
+    <Navbar collapseOnSelect expand='lg' fixed='top'>
       <Container fluid={'xl'}>
+        <img
+          alt='logo'
+          src={logo}
+          width='25'
+          height='25'
+          className='d-inline-block align-top me-3'
+        />
         <Navbar.Brand
           href='http://www.cryptnology.dev'
           target='_blank'
           rel='noopener noreferrer'
         >
-          <img
-            alt='logo'
-            src={logo}
-            width='25'
-            height='25'
-            className='d-inline-block align-top me-2'
-          />
           cryptnology.dev
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse
-          className='justify-content-end'
-          id='responsive-navbar-nav'
-        >
+        <Navbar.Collapse id='responsive-navbar-nav'>
           {account && smartContract ? (
             <>
-              <Nav>
+              <Nav className='me-auto'>
+                <Nav.Link href='#home'>Home</Nav.Link>
+                <Nav.Link href='#link'>Link</Nav.Link>
+                <Nav.Link href='#link'>Link</Nav.Link>
+                <Nav.Link href='#link'>Link</Nav.Link>
+              </Nav>
+
+              <Nav className='ms-auto'>
                 <Navbar.Text className='me-4'>{network}</Navbar.Text>
                 <Navbar.Text className='me-4'>
                   {balance} {network === 'Rinkeby' ? 'ETH' : 'MATIC'}
                 </Navbar.Text>
               </Nav>
 
-              <a
-                href={blockchainExplorerURL(account)}
-                target='_blank'
-                rel='noopener noreferrer'
-                className='btn btn-sm btn-primary me-4'
-              >
-                {account.slice(0, 5) + '...' + account.slice(38, 42)}
-              </a>
-              <Button
-                variant='dark'
-                onClick={() => {
-                  window.location.reload();
-                }}
-                className=' btn-sm'
-              >
-                Disconnect
-              </Button>
+              <div className='text-center'>
+                <a
+                  href={blockchainExplorerURL(account)}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='btn btn-sm btn-primary me-4'
+                >
+                  {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                </a>
+                <Button
+                  variant='dark'
+                  onClick={() => {
+                    window.location.reload();
+                  }}
+                  className=' btn-sm '
+                >
+                  Disconnect
+                </Button>
+              </div>
             </>
           ) : (
-            <Button
-              variant='primary'
-              onClick={e => {
-                e.preventDefault();
-                dispatch(connect());
-              }}
-              className=' btn-sm'
-            >
-              Connect Wallet
-            </Button>
+            <Nav className='ms-auto'>
+              <div className='text-center'>
+                <Button
+                  variant='primary'
+                  onClick={e => {
+                    e.preventDefault();
+                    dispatch(connect());
+                  }}
+                  className=' btn-sm me-auto'
+                >
+                  Connect Wallet
+                </Button>
+              </div>
+            </Nav>
           )}
         </Navbar.Collapse>
       </Container>
