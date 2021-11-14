40da22cb9e15ac79612df4a4194442be4597402a
Jamie Anderson <jamie@cryptnology.dev>
2021-11-14 11:06:31 +1100

    Added redux devtools and redux logger to store

--- migrations/2_smart_contract_migration.js
+++ migrations/2_smart_contract_migration.js
@@ -2,4 +2,11 @@ const SmartContract = artifacts.require('SmartContract');
 
 module.exports = function (deployer) {
   deployer.deploy(SmartContract);
+
+  /* Just an example of deploying one contract then another to use first
+  constract's address in the constructor of the second. Delete if not required.
+  */
+
+  // deployer.deploy(SmartContract).then(() => {
+  //   return deployer.deploy(SmartContract2, SmartContract.address);
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
-import { applyMiddleware, compose, createStore, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import blockchainReducer from "./blockchain/blockchainReducer";
-import dataReducer from "./data/dataReducer";
+import { applyMiddleware, compose, createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import blockchainReducer from './blockchain/blockchainReducer';
+import dataReducer from './data/dataReducer';
 
 const rootReducer = combineReducers({
   blockchain: blockchainReducer,
@@ -9,7 +11,10 @@ const rootReducer = combineReducers({
 });
 
 const middleware = [thunk];
-const composeEnhancers = compose(applyMiddleware(...middleware));
+const composeEnhancers = compose(
+  applyMiddleware(...middleware, createLogger()),
+  composeWithDevTools()
+);
 
 const configureStore = () => {
   return createStore(rootReducer, composeEnhancers);
